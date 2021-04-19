b04d0794adeae0c42165f6518d8a1de7082f3afc
AkashPatel18 <48157243+AkashPatel18@users.noreply.github.com>
2021-04-19 21:30:57 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@
  import 'firebase/firestore'
  
  var firebaseConfig = {
-    apiKey: "AIzaSyA7n_hzRt6cOjr0F41W3H4m6tkIaT0S4fM",
-    authDomain: "firegram-e191e.firebaseapp.com",
-    projectId: "firegram-e191e",
-    storageBucket: "firegram-e191e.appspot.com",
-    messagingSenderId: "386586940883",
-    appId: "1:386586940883:web:fec4d261fba6e2e563cb26"
+    ///add key
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
